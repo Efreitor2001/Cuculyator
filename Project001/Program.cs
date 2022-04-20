69d33557233807b59cd35f433cc9207bf11bc10e
Efreitor2001 <pokimoha.123@gmail.com>
2022-04-21 02:54:37 +0300

    Done

--- Project001/Program.cs
+++ Project001/Program.cs
@@ -34,6 +34,7 @@ do
         Console.WriteLine("/ - деление");
         Console.WriteLine("^ - возвести число в степень");
         Console.WriteLine("% - найти остаток от деления на второе число");
+        Console.WriteLine("# - Квадратный корень из числа");
         Console.WriteLine("-----------------------------------------------\n");
         Console.Write("Введите действие: ");
         act = Console.ReadLine();
@@ -53,6 +54,8 @@ do
             case "^":
                 return true;
             case "%":
+                return true;
+            case "#":
                 return true;
             default:
                 return false;
@@ -74,59 +77,76 @@ do
             act = Console.ReadLine();
         }
     }
-    Console.Write("\nВведите второе число: ");
-    num = Console.ReadLine();
-    while (isNum() == false)
+    if (act == "#")
     {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("\nОшибка, введите корректные данные...\n");
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.Write("Введите второе число: ");
-        num = Console.ReadLine();
-    }
-    double y = Num;
-    var result = Convert.ToDouble(0);
-    switch (act)
-    {
-        case "+":
-            result = x + y;
-            break;
-        case "-":
-            result = x - y;
-            break;
-        case "*":
-            result = x * y;
-            break;
-        case "/":Math.Pow(x, y);
-            result = x / y;
-            break;
-        case "^":
-            result = Math.Pow(x, y);
-            break;
-        case "%":
-            result = x % y;
-            break;
-        default:
-            result = 0;
-            break;
-    }
-    if (act == "/" & y == 0)
-    {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("\nНа 0 делить нельзя!\n");
-    }
-    else
-    {
+        double result = Math.Sqrt(Convert.ToDouble(num));
         Console.ForegroundColor = ConsoleColor.Blue;
         Console.WriteLine("\n---------------------------------------------");
         Console.Write("Ответ: ");
-        Console.Write(x + " " + act + " " + y + " = " + result);
+        Console.Write("Квадратный корень из " + x + " = " + result);
         Console.WriteLine("\n---------------------------------------------\n");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("Нажмите любую клавишу чтобы продолжить...");
+        Console.WriteLine("или нажмите Esc шобы остановить это безумие...");
+        Console.ForegroundColor = ConsoleColor.Green;
     }
-    Console.ForegroundColor = ConsoleColor.White;
-    Console.WriteLine("Нажмите любую клавишу чтобы продолжить...");
-    Console.WriteLine("или нажмите Esc шобы остановить это безумие...");
-    Console.ForegroundColor = ConsoleColor.Green;
+    else
+    {
+        Console.Write("\nВведите второе число: ");
+        num = Console.ReadLine();
+        while (isNum() == false)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\nОшибка, введите корректные данные...\n");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Введите второе число: ");
+            num = Console.ReadLine();
+        }
+        double y = Num;
+        var result = Convert.ToDouble(0);
+        switch (act)
+        {
+            case "+":
+                result = x + y;
+                break;
+            case "-":
+                result = x - y;
+                break;
+            case "*":
+                result = x * y;
+                break;
+            case "/":
+                Math.Pow(x, y);
+                result = x / y;
+                break;
+            case "^":
+                result = Math.Pow(x, y);
+                break;
+            case "%":
+                result = x % y;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+        if (act == "/" & y == 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\nНа 0 делить нельзя!\n");
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\n---------------------------------------------");
+            Console.Write("Ответ: ");
+            Console.Write(x + " " + act + " " + y + " = " + result);
+            Console.WriteLine("\n---------------------------------------------\n");
+        }
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("Нажмите любую клавишу чтобы продолжить...");
+        Console.WriteLine("или нажмите Esc шобы остановить это безумие...");
+        Console.ForegroundColor = ConsoleColor.Green;
+    }
 }
 while (Console.ReadKey(true).Key != ConsoleKey.Escape);
 Console.WriteLine("Пока-пока");
